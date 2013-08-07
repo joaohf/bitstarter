e7dc41e8026b0984277d5b9ebe104b6d7e0e27de
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2013-08-07 19:53:30 -0300

    Change default port

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express(express.logger());
 
 app.get('/', function(request, response) {
   var buf = fs.readFileSync('index.html');
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
   response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
